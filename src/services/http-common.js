c76c8b712c41ff33e48a2708f18486ccc4c7d973
zaj-e <carbonelmarcelo@gmail.com>
2020-11-27 21:03:54 -0500

    fixin

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://offirent.azurewebsites.net/api',
     headers: {
         'Content-type': 'application/json',
     }
